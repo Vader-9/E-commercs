58eb0170a3892907e16c976e35b8ea5faf4d71a6
Vader <thesecondshot7@gmail.com>
2025-10-02 14:48:03 -0700

    now header done

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -2,13 +2,13 @@ import { assets } from "../assets/assets"
 
 function Header(){
     return(
-        <div className="flex border h-100 justify-center items-center">
-            <div className="p-[100] w-50%">
+        <div className="flex border h-150 justify-center items-center gap-130">
+            <div className="p-[100] w-50% text-center">
                 <h1>_______Welcome to Amazo</h1>
                 <h1>Latest Arrivals</h1>
                 <h1>SHOP NOW_________</h1>
             </div>
-            <img src={assets.hero_img} alt="" className="border w-50% " />
+            <img src={assets.hero_img} alt="" className=" w-50% h-[100%] " />
         </div>
     )
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function Nav(){
 
     return(
-        <div className='flex items-center gap-[270px] justify-between py-2 font-medium border-b'>
+        <div className='flex items-center gap-[270px] justify-between py-2 font-medium '>
             <img src={assets.logo} alt="" />
             <div className='flex gap-[10px] items-center'>
                 <Link to="/">Home</Link>
