ea8858411bd8b6157b3e7ef3842c0dd5315aa631
Vader <thesecondshot7@gmail.com>
2025-10-14 12:20:05 -0700

    small change

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -2,7 +2,7 @@ import { assets } from "../assets/assets"
 
 function Header() {
     return (
-        <div className="w-full h-[400px] flex justify-center items-center bg-gray-50">
+        <div className="w-full h-[300px] flex justify-center items-center bg-gray-50 mt-[20px]">
             <div className="flex flex-col gap-[10px]   md:flex-row md:justify-between md:items-center  sm:px-6 md:px-10  bg-gray-50 md:pt-[0px] md:h-[600px] md: w-[90%] md: border border-gray-200 md:p-[0px] md: gap-[100px]">  
             <div className="p-[100] w-50% text-center md: w-full md:gap-[50px] md:text-left">
                 <h1>_______Welcome to Amazo</h1>
