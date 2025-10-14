42ee2f599bfa6a8859f5287be65edbd984cf7d56
Vader <thesecondshot7@gmail.com>
2025-10-14 11:56:07 -0700

    size of nav icons adjusted

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -12,13 +12,14 @@ function Nav({ setSearch, addToCart, setActive }) {
                 <Link to='Collection'>Collection</Link>
                 <Link to='About'>About</Link>
                 <Link to='Contact'>Contact</Link>
-                <Link to ="Admin"><button>Admin panel</button></Link>
+                <Link to ="Admin"><button>Login/sign up</button></Link>
             </div>
-            <div className='flex gap-[10px]'>
-                <Link to= 'Collection'><img src={assets.search_icon} alt="" onClick={()=> setSearch(true)} className="w-8 h-8 "  /></Link>
-                <Link to = 'Profile'><img src={assets.profile_icon} alt=""  className="w-8 h-8"  /></Link>
-                <Link to='Cart'><div className="relative inline-block">
-                    <img src={assets.cart_icon} alt="cart" className="w-8 h-8" />
+            <div className='flex gap-[10px] items-center'>
+                <Link to= 'Collection'><img src={assets.search_icon} alt="" onClick={()=> setSearch(true)} className="w-5 h-5 "  /></Link>
+                <Link to = 'Profile'><img src={assets.profile_icon} alt=""  className="w-5 h-5"  /></Link>
+                <Link to='Cart'>
+                <div className="relative inline-block">
+                    <img src={assets.cart_icon} alt="cart" className="w-5 h-5" />
                     {addToCart.length > 0 && (
                         <div className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold px-1.5 py-0.5 rounded-full">
                             {addToCart.length}
@@ -26,7 +27,7 @@ function Nav({ setSearch, addToCart, setActive }) {
                     )}
                 </div>
                 </Link>
-                <EllipsisVertical className='md:hidden w-8 h-8' onClick={()=>setActive(true)}/>
+                <EllipsisVertical className='md:hidden w-5 h-5' onClick={()=>setActive(true)}/>
             </div>
         </div>
     )
