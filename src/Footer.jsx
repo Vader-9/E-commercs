b8e93830108464096c276fd26a724def577b8cd9
Vader <thesecondshot7@gmail.com>
2025-10-14 16:57:27 -0700

    the pad

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,11 +3,11 @@ import {assets} from './assets/assets'
 function Footer(){
 
     return(
-       <div className='mt-[70px] md:px-[100px] px-[20px]  pt-[30px] pb-[20px] sm:px-[50px] '>
+       <div className='mt-[70px] mx-[0px] w-full bg-blue-500 md:px-[100px] px-[20px]  pt-[30px] pb-[20px] sm:px-[50px] '>
         <div className=' md:flex '>
             <div className='w-[33%]   md:w-[33%] sm:w-[50%] w-[100%] '>
                 <img src={assets.logo} alt=""  className='w-[100px] p-[20px]'/>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicin explicabo voluptatum accusantium nesciunt reprehenderit architecto maxime sed amet deleniti aliquid voluptas ab debitis laboriosam quos.</p>
+                <p className='text-white md:text-black'>Lorem ipsum dolor, sit amet consectetur adipisicin explicabo voluptatum accusantium nesciunt reprehenderit architecto maxime sed amet deleniti aliquid voluptas ab debitis laboriosam quos.</p>
             </div>
             <div className='w-[33%]  p-[20px]'>
                 <h1>COMPANY</h1>
