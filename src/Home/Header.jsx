2c516e7814d595da742751950ac69da4632bf2d5
Vader <thesecondshot7@gmail.com>
2025-10-14 12:55:16 -0700

    omom men

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -2,14 +2,14 @@ import { assets } from "../assets/assets"
 
 function Header() {
     return (
-        <div className="w-full h-[600px] flex justify-center items-center bg-gray-50">
+        <div className="w-full h-[600px] flex justify-center items-center bg-gray-50 m-[20px]">
             <div className="flex flex-col   h-[400px] mb-[30px]  md:flex-row md:justify-between md:items-center  sm:px-2 md:px-5  bg-gray-50 md:pt-[0px] md:h-[600px] md: w-[90%] md: border border-gray-200 md:p-[0px] md: gap-[100px]">  
-            <div className=" w-50% text-center md: w-full md:gap-[50px] md:text-left">
+            <div className=" w-50% text-center md:w-full md:gap-[50px] md:text-left">
                 <h1>_______Welcome to Amazo</h1>
                 <h1>Latest Arrivals</h1>
                 <h1>SHOP NOW_________</h1> 
             </div>
-            <img src={assets.hero_img} alt="" className="w-full   md:w-full md:h-full   " />
+            <img src={assets.hero_img} alt="" className="w-full h-[60px]  md:w-full md:h-full   " />
         </div>
         </div>
 
